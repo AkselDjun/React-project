d5669eabc0d8c0d92a75cc19a478c71c1ad1f8e4
AkselDjun <akseldjun@gmail.com>
2020-06-09 19:00:18 +0300

    Q&A bock create

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,39 +1,147 @@
 import React, { Component } from 'react';
 import classes from "./quiz.module.css"
 import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz"
+import FinishedQuiz from "../../components/FinishedQuiz/FinishedQuiz"
+
 
 class Quiz extends Component {
     state = {
+        results: {}, // {[id]: "success" "error"}
+        isFinished: false,
+        activeQuestion: 0,
+        answerState: null, //{ [id]: "success" "error"}
         quiz: [
             {
                 question: "Какого цвета небо?",
                 rightAnswerId: 2,
+                id: 1,
                 answers: [
                     { text: "Черного", id: 1 },
                     { text: "Синего", id: 2 },
                     { text: "Красного", id: 3 },
                     { text: "Розового", id: 4 },
                 ]
+            },
+
+            {
+                question: "В какую пору года холодно?",
+                rightAnswerId: 4,
+                id: 2,
+                answers: [
+                    { text: "Летом", id: 1 },
+                    { text: "Осенью", id: 2 },
+                    { text: "Весной", id: 3 },
+                    { text: "Зимой", id: 4 },
+                ]
+            },
+
+            {
+                question: "В каком году началась Великая Отечественная Война?",
+                rightAnswerId: 1,
+                id: 3,
+                answers: [
+                    { text: 1941, id: 1 },
+                    { text: 1945, id: 2 },
+                    { text: 1939, id: 3 },
+                    { text: 1918, id: 4 },
+                ]
+            },
+
+            {
+                question: "В каком году затонул Титаник?",
+                rightAnswerId: 4,
+                id: 4,
+                answers: [
+                    { text: 1910, id: 1 },
+                    { text: 1914, id: 2 },
+                    { text: 1908, id: 3 },
+                    { text: 1912, id: 4 },
+                ]
             }
         ]
     }
 
     onAnswerClickHandler = (answerId) => {
-        console.log("answer Id: ", answerId)
+        if (this.state.answerState) {
+            const key = Object.keys(this.state.answerState)[0]
+            if (this.state.answerState[key] === "success") {
+                return
+            }
+        }
+
+        const question = this.state.quiz[this.state.activeQuestion]
+        const results = this.state.results
+
+        if (question.rightAnswerId === answerId) {
+            if (!results[question.id]) {
+                results[question.id] = "success"
+            }
+
+            this.setState({
+                answerState: { [answerId]: "success" },
+                results
+            })
+
+            const timeout = window.setTimeout(() => {
+                if (this.isQuizFinished()) {
+                    this.setState({
+                        isFinished: true
+                    })
+                } else {
+                    this.setState({
+                        activeQuestion: this.state.activeQuestion + 1,
+                        answerState: null
+                    })
+                }
+
+                window.clearTimeout(timeout)
+            }, 500)
+
+        } else {
+            results[question.id] = "error"
+            this.setState({
+                answerState: { [answerId]: "error" },
+                results
+            })
+        }
+    }
+
+    isQuizFinished() {
+        return this.state.activeQuestion + 1 === this.state.quiz.length
+    }
+
+    retryHandler = () => {
+        this.setState({
+            activeQuestion: 0,
+            answerState: null,
+            isFinished: false,
+            results: {}
+        })
     }
 
     render() {
         return (
             <div className={classes.Quiz}>
-
                 <div className={classes.QuizWrapper}>
                     <h1>Ответьте на все вопросы</h1>
 
-                    <ActiveQuiz
-                        answers={this.state.quiz[0].answers}
-                        question={this.state.quiz[0].question}
-                        onAnswerClick={this.onAnswerClickHandler}
-                    />
+
+                    {
+                        this.state.isFinished
+                            ? <FinishedQuiz
+                                results={this.state.results}
+                                quiz={this.state.quiz}
+                                onRetry={this.retryHandler}
+                            />
+                            : <ActiveQuiz
+                                answers={this.state.quiz[this.state.activeQuestion].answers}
+                                question={this.state.quiz[this.state.activeQuestion].question}
+                                onAnswerClick={this.onAnswerClickHandler}
+                                quizLength={this.state.quiz.length}
+                                answerNumber={this.state.activeQuestion + 1}
+                                state={this.state.answerState}
+                            />
+                    }
                 </div>
             </div>
         )
